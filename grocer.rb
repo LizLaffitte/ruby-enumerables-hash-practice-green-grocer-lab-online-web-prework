7cf47feb3c05ca12dad1adf6ac7082c8ab29956e
Liz Laffitte <liz.kossakowski@gmail.com>
2019-09-11 14:45:34 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -15,6 +15,7 @@ def apply_coupons(cart, coupons= {})
   # code here
   new_prices = {}
   cart.each do |grocery_item, item_details|
+<<<<<<< HEAD
     discounted_item = grocery_item.dup + " W/COUPON"
     discounted_details = {:price => nil, :clearance => nil, :count =>nil}
     coupons.each do |coupon| 
@@ -24,15 +25,31 @@ def apply_coupons(cart, coupons= {})
 	      discounted_details[:count] += coupon[:num]
         discounted_details[:clearance] = item_details[:clearance].dup
         item_details[:count] -= coupon[:num]
+=======
+    coupons.each do |coupon| 
+      if coupon[:item] == grocery_item && item_details[:count] > 0
+        discounted_item ||= grocery_item.dup + " W/C"
+	      discounted_details ||= {}
+	      discounted_details[:price] ||= (coupon[:cost] / coupon[:num])
+	      discounted_details[:count] ||= 0
+	      discounted_details[:count] += coupon[:num]
+        discounted_details[:clearance] = item_details[:clearance].dup
+        discounted_details[:count]
+        item_details[:count] -= discounted_details[:count]
+>>>>>>> aecd95e01952f9f2555697eefd84a7baefb6f143
         new_prices[grocery_item] = item_details
         new_prices[discounted_item] = discounted_details
       end
     end
+<<<<<<< HEAD
     
+=======
+>>>>>>> aecd95e01952f9f2555697eefd84a7baefb6f143
   new_prices[grocery_item] ||= item_details
   end
   new_prices
 end
+<<<<<<< HEAD
 
 def apply_clearance(cart)
   # code here
@@ -67,3 +84,17 @@ puts checkout(
   {"AVOCADO" => {:price => 3.00, :clearance => true }},
   {"KALE"    => {:price => 3.00, :clearance => false}}
 ],[{:item => "AVOCADO", :num => 2, :cost => 5.00},{:item => "BEER", :num => 2, :cost => 20.00}, {:item => "CHEESE", :num => 3, :cost => 15.00}])
+=======
+puts apply_coupons({
+  "AVOCADO" => {:price => 3.00, :clearance => true, :count => 4},
+  "KALE"    => {:price => 3.00, :clearance => false, :count => 1}
+},[{:item => "AVOCADO", :num => 2, :cost => 5.00},{:item => "AVOCADO", :num => 2, :cost => 5.00},{:item => "AVOCADO", :num => 2, :cost => 5.00}])
+
+def apply_clearance(cart)
+  # code here
+end
+
+def checkout(cart, coupons)
+  # code here
+end
+>>>>>>> aecd95e01952f9f2555697eefd84a7baefb6f143
